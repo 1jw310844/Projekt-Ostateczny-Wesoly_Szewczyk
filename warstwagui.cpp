5d04c40a3fd548e17bb6fb5839ae0665df4f9654
1jw310844 <149474616+1jw310844@users.noreply.github.com>
2025-01-31 09:52:32 +0100

    Update warstwagui.cpp

--- warstwagui.cpp
+++ warstwagui.cpp
@@ -6,12 +6,10 @@ WarstwaGUI::WarstwaGUI(QObject *parent)
 {}
 void WarstwaGUI::setSpinBoxValue(double value)
 {
-    qDebug() << "Ustawiam wartość SpinBoxa na:" << value;
     emit valueChanged(value);
 }
 
 void WarstwaGUI::handleSpinBoxValueChanged(double value)
 {
     qDebug() << "Wartość SpinBox zmieniła się na:" << value;
-    // Możesz dodać dodatkową logikę obsługującą zmianę wartości
 }
